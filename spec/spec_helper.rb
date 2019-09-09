e03fa2e30592ac8d8c6c50d6d9e0f049291983e6
carlos-oliver <carlos.st.oliveira@gmail.com>
2019-09-09 17:01:20 -0300

    2 - Exemplos usando Capybara:
    - checkbox
    - dropdown_spec
    - dynamic_control
    - forms
    - hovers
    - radio
    - upload

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,17 @@ require "selenium-webdriver"
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+end
 
-​  config.mock_with :rspec do |mocks|
+config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
-  end
+end
 
-​  config.shared_context_metadata_behavior = :apply_to_host_groups
+config.shared_context_metadata_behavior = :apply_to_host_groups
   config.include Capybara::DSL
 end
 
-​Capybara.configure do |config|
+Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.default_max_wait_time = 15
   config.app_host = 'https://training-wheels-protocol.herokuapp.com'
